bae1264c36104873070890de4c1626806340e39f
robertoborges98 <robbyborges98@outlook.com>
2021-12-16 11:07:21 -0500

    updated addComment method so comments can be inserted into mongoDB

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -45,7 +45,14 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: Create/Update Comments
       // Construct the comment document to be inserted into MongoDB.
-      const commentDoc = { someField: "someValue" }
+      const commentDoc = { 
+        name: user.name,
+        email: user.email,
+        movie_id: ObjectId(movieId),
+        text: comment,
+        date: date
+       }
+                          
 
       return await comments.insertOne(commentDoc)
     } catch (e) {
@@ -69,9 +76,16 @@ export default class CommentsDAO {
       // TODO Ticket: Create/Update Comments
       // Use the commentId and userEmail to select the proper comment, then
       // update the "text" and "date" fields of the selected comment.
-      const updateResponse = await comments.updateOne(
-        { someField: "someValue" },
-        { $set: { someOtherField: "someOtherValue" } },
+      const updateResponse = await comments.updateOne({ 
+        _id: ObjectId(commentId),
+        email: userEmail
+      },{
+        $set: {
+          text,
+          date
+        }
+      },
+        
       )
 
       return updateResponse
@@ -96,6 +110,7 @@ export default class CommentsDAO {
       // Use the userEmail and commentId to delete the proper comment.
       const deleteResponse = await comments.deleteOne({
         _id: ObjectId(commentId),
+        email: userEmail
       })
 
       return deleteResponse
@@ -105,34 +120,43 @@ export default class CommentsDAO {
     }
   }
 
+  
   static async mostActiveCommenters() {
-    /**
-    Ticket: User Report
-
-    Build a pipeline that returns the 20 most frequent commenters on the MFlix
-    site. You can do this by counting the number of occurrences of a user's
-    email in the `comments` collection.
-    */
-    try {
-      // TODO Ticket: User Report
-      // Return the 20 users who have commented the most on MFlix.
-      const pipeline = []
-
-      // TODO Ticket: User Report
-      // Use a more durable Read Concern here to make sure this data is not stale.
-      const readConcern = comments.readConcern
-
-      const aggregateResult = await comments.aggregate(pipeline, {
-        readConcern,
-      })
-
-      return await aggregateResult.toArray()
-    } catch (e) {
-      console.error(`Unable to retrieve most active commenters: ${e}`)
-      return { error: e }
+      /**
+      Ticket: User Report
+      Build a pipeline that returns the 20 most frequent commenters on the MFlix
+      site. You can do this by counting the number of occurrences of a user's
+      email in the `comments` collection.
+      */
+      try {
+        // TODO Ticket: User Report
+        // Return the 20 users who have commented the most on MFlix.
+        
+        // here's how the pipeline stages are assembled
+        const groupStage = { $group: { _id: "$email", count: { $sum: 1 } } }
+        const sortStage = { $sort: { count: -1 } }
+        const limitStage = { $limit: 20 }
+        const pipeline = [groupStage, sortStage, limitStage]
+  
+        // TODO Ticket: User Report
+        // Use a more durable Read Concern here to make sure this data is not stale.
+  
+        // here's how the Read Concern durability is increased
+        const readConcern = comments.readConcern
+  
+        const aggregateResult = await comments.aggregate(pipeline, {
+          readConcern,
+        })
+  
+        return await aggregateResult.toArray()
+      } catch (e) {
+        console.error(`Unable to retrieve most active commenters: ${e}`)
+        return {
+          error: e
+        }
+      }
     }
   }
-}
 
 /**
  * Success/Error return object
